acdf3beb35032c6af3d59eb4c595d402a8cc55ef
Harshil Dhariwal <harshil.dhariwal@volkswagen.co.in>
2017-09-25 15:12:24 +0530

    updated

---
+++ src/main/java/vw/training/SpringConfig.java
@@ -0,0 +1,58 @@
+package vw.training;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+
+import javax.sql.DataSource;
+import java.util.Properties;
+
+/**
+ * Created by digitallab on 25/09/17.
+ */
+
+@Configuration
+@SpringBootApplication
+@EnableJpaRepositories(basePackages = "vw.training")
+@EnableSpringDataWebSupport
+public class SpringConfig {
+
+    @Bean
+    public LocalContainerEntityManagerFactoryBean entityManagerFactoryBean(DataSource dataSource){
+        LocalContainerEntityManagerFactoryBean factory = new LocalContainerEntityManagerFactoryBean();
+        factory.setDataSource(dataSource);
+        factory.setPackagesToScan("vw.training");
+        factory.setJpaVendorAdapter(new HibernateJpaVendorAdapter());
+
+        factory.setJpaProperties(hibernateProperties());
+        return factory;
+    }
+
+    @Bean
+    public DataSource dataSource(){
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setUrl("jdbc:h2:file:~/test");
+        dataSource.setDriverClassName("org.h2.Driver");
+        return dataSource;
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringConfig.class,args);
+    }
+
+    Properties hibernateProperties() {
+        return new Properties() {
+            {
+                setProperty("hibernate.hbm2ddl.auto", "create");
+                setProperty("hibernate.dialect", "org.hibernate.dialect.H2Dialect");
+                setProperty("hibernate.globally_quoted_identifiers", "true");
+            }
+        };
+    }
+}
